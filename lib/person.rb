f14a753c8ae48023cc82c83688862bb104a6aaf9
Tirzah P. Howard <tp.howard47@gmail.com>
2020-09-24 18:02:36 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,14 +1,8 @@
 class Person
-  def name(name)
-    @name=name
-  end
-  def name
-    @name
-  end
-  def job(job)
-    @job=job
-  end
-  def job
-  @job
-  end
-end
+  def name=(name)
+     @name = name
+   end
+   def name
+     @name
+   end
+  
